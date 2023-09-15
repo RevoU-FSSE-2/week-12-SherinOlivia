17522e319b53c31aa135c5a158d81c3ece8935e7
Sherin Olivia <SOChronicle@gmail.com>
2023-09-15 07:16:09 +0700

    feat: implemented steppers

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,29 @@
 // import { useState } from 'react'
 import React, { useState} from 'react'
-import { PersonalInformation, AddressInformation } from './containers'
+import { SubmitButton } from './components';
+import { PersonalInformation, AddressInformation, AccountInformation } from './containers'
+import { Space, Button } from 'antd';
 import './App.css'
 
+
 function App() {
-const { step, setStep } = useState<number>(1);
+  const [step, setStep] = useState<number>(1);
 
-const handleNext  = () => {
+  const handleNext = () => {
+    if(step === 1 || step === 2) {
+        setStep((prevStep) => prevStep+1);
+    }
 
-}
+    return
+  }
+
+  const handlePrev = () => {
+    if(step === 2 || step === 3) {
+        setStep((prevStep) => prevStep - 1);
+    }
+
+    return
+  }
 
   return (
     <>
@@ -18,8 +33,36 @@ const handleNext  = () => {
       {step === 2 && (
         <AddressInformation />
       )}
-      </>
+      {step === 3 && (
+        <AccountInformation />
+      )}
 
+
+      <Space direction="vertical">
+        <Space wrap>
+
+          {step === 2 && (
+            <div>
+              <Button onClick={handlePrev}>Previous</Button>
+              <Button  onClick={handleNext} type={'primary'}>Next</Button>
+            </div>
+          )}
+
+          {step === 1 && (
+            <Button  onClick={handleNext} type={'primary'}>Next</Button>
+          )}
+
+          {step === 3 && (
+            <div>
+              <Button onClick={handlePrev}>Previous</Button>
+              <SubmitButton />
+            </div>
+
+          )}
+
+        </Space>
+      </Space>
+    </>
       )
 }
 
