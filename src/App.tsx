efef61fff858e2a20b945b408a6cf35718799294
Sherin Olivia <SOChronicle@gmail.com>
2023-09-16 05:53:12 +0700

    fix: app.tsx only calls RegistrationForm

--- src/App.tsx
+++ src/App.tsx
@@ -1,69 +1,13 @@
-// import { useState } from 'react'
-import React, { useState} from 'react'
-import { SubmitButton } from './components';
-import { PersonalInformation, AddressInformation, AccountInformation } from './containers'
-import { Space, Button } from 'antd';
+import { RegistrationForm } from './containers'
 import './App.css'
 
-
-function App() {
-  const [step, setStep] = useState<number>(1);
-
-  const handleNext = () => {
-    if(step === 1 || step === 2) {
-        setStep((prevStep) => prevStep+1);
-    }
-
-    return
-  }
-
-  const handlePrev = () => {
-    if(step === 2 || step === 3) {
-        setStep((prevStep) => prevStep - 1);
-    }
-
-    return
-  }
+const App = () => {
 
   return (
-    <>
-      {step === 1 && (
-        <PersonalInformation />
-      )}
-      {step === 2 && (
-        <AddressInformation />
-      )}
-      {step === 3 && (
-        <AccountInformation />
-      )}
 
-
-      <Space direction="vertical">
-        <Space wrap>
-
-          {step === 2 && (
-            <div>
-              <Button onClick={handlePrev}>Previous</Button>
-              <Button  onClick={handleNext} type={'primary'}>Next</Button>
-            </div>
-          )}
-
-          {step === 1 && (
-            <Button  onClick={handleNext} type={'primary'}>Next</Button>
-          )}
-
-          {step === 3 && (
-            <div>
-              <Button onClick={handlePrev}>Previous</Button>
-              <SubmitButton />
-            </div>
-
-          )}
-
-        </Space>
-      </Space>
-    </>
-      )
+    <RegistrationForm />
+  
+  )
 }
 
 export default App
